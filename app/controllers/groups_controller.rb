e88628851253a8ac8da2778f4f15a7a30003c0a1
sdlong <sdlong.jeng@gmail.com>
2013-12-26 05:53:20 +0800

    create done

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,10 @@ class GroupsController < ApplicationController
   end
 
   def create
+	@group = Group.new(group_params)
+	@group.save
+
+	redirect_to groups_path
   end
 
   def update
@@ -22,4 +26,12 @@ class GroupsController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def group_params
+   params.require(:group).permit(:title, :description)
+  end
+
 end
+
